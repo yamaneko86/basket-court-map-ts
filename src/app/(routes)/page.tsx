ae9fdc2d310107036f0e1368ee554ec60f144d0a
yamaneko86 <yoshibasket86@gmail.com>
2023-12-30 17:35:36 +0900

    ディレクトリ構成の変更（componentsをプライベート(ファイル名先頭にアンダースコア)に、ユーザーが見る画面を(routes)内に格納）

---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header from "../_components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <h1>こんにちは</h1>
+    </>
+  );
+}
---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>これはヘッダーです。</div>;
+};
+
+export default Header;
