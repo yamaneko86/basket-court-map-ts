354e98c1832a7ff92292440cb40acacff804c9b7
yamaneko86 <yoshibasket86@gmail.com>
2023-12-30 17:54:44 +0900

    aboutページへのリンクを追加

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Header from "../_components/Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
       <h1>こんにちは</h1>
+      <Link href={"/about"}>
+        <h2>このアプリについて</h2>
+      </Link>
     </>
   );
 }
