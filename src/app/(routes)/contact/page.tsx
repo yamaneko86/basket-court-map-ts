17659919feb4e58299e0c0f1b278c62e5ef2cf95
yamaneko86 <yoshibasket86@gmail.com>
2024-01-03 15:02:36 +0900

    💄 contactページの追加

---
+++ src/app/(routes)/contact/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "~/app/_components/Footer";
+import Header from "~/app/_components/Header";
+
+export default function Contact() {
+  return (
+    <main className="flex flex-col h-screen">
+      <Header />
+      <h1>これはcontactページです</h1>
+      <Footer />
+    </main>
+  );
+}
