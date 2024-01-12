fe7cd6f439ffa2a04ea92f9d77b145209ba9b831
yamaneko86 <yamaneko8699@gmail.com>
2024-01-13 02:28:42 +0900

    ✨ 都道府県番号で範囲を指定して、対象のコート数を取得する関数を追加

---
+++ src/app/_lib/supabaseFunc.ts
@@ -0,0 +1,25 @@
+import { supabase } from "./supabase";
+
+// 都道府県をグループ化してコート件数を取得
+export const getCountOfCourts = async (
+  lower_limit: number,
+  upper_limit: number
+) => {
+  try {
+    const { count, error } = await supabase
+      .from("BasketCourtMaps")
+      .select("*", { count: "exact", head: true })
+      .gte("prefecture_code", lower_limit)
+      .lte("prefecture_code", upper_limit);
+
+    if (error) {
+      throw error;
+    }
+
+    // コートの数を返す
+    return count;
+  } catch (error) {
+    console.error("Error fetching count");
+    return -1;
+  }
+};
