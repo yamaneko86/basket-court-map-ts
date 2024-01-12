3d227a7fcb81c392226192b0639a26b63761d045
yamaneko86 <yamaneko8699@gmail.com>
2024-01-13 02:26:36 +0900

    ✨ コート数の表示（仮）

--- src/app/_components/MapApp.tsx
+++ src/app/_components/MapApp.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { getCountOfCourts } from "@/_lib/supabaseFunc";
 
 const MapApp = () => {
-  return <div>MapApp</div>;
+  const [count, setCount] = useState<any>();
+
+  useEffect(() => {
+    const getKyusyuCourts = async () => {
+      const count = await getCountOfCourts(40, 47);
+      // console.log(count);
+      setCount(count);
+    };
+    getKyusyuCourts();
+  }, []);
+
+  return (
+    <div className="flex justify-center text-red-500">
+      九州地方のコート数：{count}
+    </div>
+  );
 };
 
 export default MapApp;
