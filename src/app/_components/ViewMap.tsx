fd07b311164ccc79294e0634e60cc90bcfe739f1
yamaneko86 <yamaneko8699@gmail.com>
2024-01-20 20:56:12 +0900

    ✨ 地図に現在地、目的地、2点間の距離を表示

--- src/app/_components/ViewMap.tsx
+++ src/app/_components/ViewMap.tsx
@@ -1,44 +1,85 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import { useEffect } from "react";
+"use client";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
+import { getCourtLatLng } from "@/_lib/supabaseFunc";
+import { distanceCalc } from "@/_lib/distanceCalc";
 
 const containerStyle = {
   width: "80%",
   height: "75vh",
 };
 
-let center = {
-  // 初期座標：ユーザの位置情報
-  lat: 34.7293466708865,
-  lng: 135.49939605607292,
-};
+const zoomScale = 15;
 
-const zoom = 13;
+let distance: number;
 
 const ViewMap = () => {
+  // 現在地の緯度経度を管理
+  const [userPos, setUserPos] = useState<{ lat: number; lng: number }>({
+    lat: 0,
+    lng: 0,
+  });
+
+  // 目的地の緯度経度を管理
+  const [courtPos, setCourtPos] = useState<{ lat: number; lng: number }>({
+    lat: 0,
+    lng: 0,
+  });
+
   useEffect(() => {
     // ユーザの現在の緯度経度を取得
-    const getUserLatLon = () => {
-      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-      // 取得成功処理
-      function successCallback(position: any) {
-        const lat: number = position.coords.latitude;
-        const lon: number = position.coords.longitude;
-        center.lat = lat;
-        center.lng = lon;
-      }
-
-      // 取得失敗処理
-      function errorCallback(error: any) {
-        alert("位置情報を取得できませんでした。");
-      }
+    const getUserLatLng = () => {
+      navigator.geolocation.getCurrentPosition(
+        // 取得成功処理
+        (position: GeolocationPosition) => {
+          const curLat: number = position.coords.latitude;
+          const curLng: number = position.coords.longitude;
+          setUserPos({ lat: curLat, lng: curLng });
+        },
+        // 取得失敗処理
+        (error: any) => {
+          alert("位置情報を取得できませんでした。");
+        }
+      );
     };
-    getUserLatLon();
-  }, []);
+
+    // コートの緯度経度を取得
+    const getPlaceLatLng = async () => {
+      const data = await getCourtLatLng("370c1246-753a-4178-bc56-c44e923d8a00");
+      if (data) {
+        setCourtPos({ lat: data[0].latitude, lng: data[0].longitude });
+      }
+      // setCourtPos({ lat: 33.589728, lng: 130.420727 });
+    };
+
+    // 処理呼び出し
+    getUserLatLng();
+    getPlaceLatLng();
+    distance = distanceCalc(
+      userPos.lat,
+      userPos.lng,
+      courtPos.lat,
+      courtPos.lng
+    );
+  }, [courtPos.lat, courtPos.lng, userPos.lat, userPos.lng]);
 
   return (
-    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} />
-    </LoadScript>
+    <div>
+      <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={userPos}
+          zoom={zoomScale}
+        >
+          <MarkerF visible={true} position={userPos} />
+          <MarkerF visible={true} position={courtPos} />
+        </GoogleMap>
+      </LoadScript>
+      <div>2点間の距離:{distance}km</div>
+    </div>
   );
 };
+
+export default ViewMap;
