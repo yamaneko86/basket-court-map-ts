c91ee511bd82166ed36fc6a727dec466b67c0f6e
yamaneko86 <yamaneko8699@gmail.com>
2024-01-21 23:11:39 +0900

    💄 九州ページのコンポーネント修正

--- src/app/(routes)/kyushu/page.tsx
+++ src/app/(routes)/kyushu/page.tsx
@@ -1,13 +1,13 @@
+import CourtsList from "@/_components/CourtsList";
 import Footer from "@/_components/Footer";
 import Header from "@/_components/Header";
-import ViewMap from "@/_components/ViewMap";
 
 export default function Kyushu() {
   return (
     <div>
       <main className="flex flex-col h-screen">
         <Header />
-        <ViewMap />
+        <CourtsList lower_limit={40} upper_limit={47} />
         <Footer />
       </main>
     </div>
