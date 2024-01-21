541ed4e690f4bccd6911ab3ca6479217e6458a11
yamaneko86 <yamaneko8699@gmail.com>
2024-01-22 00:19:31 +0900

    ✨ インターフェースの追加

--- src/app/_lib/interface.ts
+++ src/app/_lib/interface.ts
@@ -1,9 +1,15 @@
+// マップIDを受け取る時のインターフェース
+interface MapId {
+  map_id: string;
+}
+
 // 都道府県コードを受け取る時のインターフェース
 interface PrefectureCodes {
   lower_limit: number;
   upper_limit: number;
 }
 
+// コート情報を受け取る時のインターフェース
 interface CourtInfo {
   map_id: string;
   prefecture_code: number;
