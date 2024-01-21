b6d162b401aa9aa4fa220bfccf9b8d135c03cc81
yamaneko86 <yamaneko8699@gmail.com>
2024-01-21 23:12:40 +0900

    ✨ インターフェースファイルを更新

--- src/app/_lib/interface.ts
+++ src/app/_lib/interface.ts
@@ -1,3 +1,19 @@
+// 都道府県コードを受け取る時のインターフェース
+interface PrefectureCodes {
+  lower_limit: number;
+  upper_limit: number;
+}
+
+interface CourtInfo {
+  map_id: string;
+  prefecture_code: number;
+  map_name: string;
+  map_address: string;
+  latitude: number;
+  longitude: number;
+  isUsing: boolean;
+}
+
 // interface LatLng {
 //   latitude: number;
 //   longitude: number;
