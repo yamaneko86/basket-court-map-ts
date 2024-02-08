be50379c98000be685ff28477b48491f8dd8e2a9
yamaneko86 <yamaneko8699@gmail.com>
2024-02-08 20:10:52 +0900

    🔥 console.logの削除

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,10 +1,8 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const requestUrl = new URL(request.url)
-
-    console.log(requestUrl.origin)
-    return NextResponse.redirect(requestUrl.origin + '/')
+  const requestUrl = new URL(request.url);
+  return NextResponse.redirect(requestUrl.origin + "/");
 }
