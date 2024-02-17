7f16e069a86b6c4c0c6ac7fafb913cf5ae71bb09
yamaneko86 <yamaneko8699@gmail.com>
2024-02-17 11:42:42 +0900

    例文を追加

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,6 +6,12 @@ export default function Contact() {
     <main className="flex flex-col h-screen">
       <SupabaseListener />
       <h1>これはcontactページです</h1>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel est quos
+        voluptas labore repellendus accusamus. Aliquid facere laborum expedita
+        numquam reprehenderit maiores tempora nobis nemo. Numquam minima ea
+        explicabo veniam.
+      </div>
       <Footer />
     </main>
   );
