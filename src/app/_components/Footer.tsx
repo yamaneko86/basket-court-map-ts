fb62651ae48befbce780cea503141f5dde422349
yamaneko86 <yamaneko8699@gmail.com>
2024-02-20 21:19:25 +0900

    トップに戻るボタンを削除

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -4,11 +4,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="flex items-center mt-auto h-12 bg-amber-500">
-      <Link href={"/"} className="pl-4 pr-4">
-        トップへ戻る
-      </Link>
-      <div className="ml-auto pr-4 text-black">
-        Developed by{" "}
+      <div className="ml-auto px-4 text-black">
+        © 2024 Developed by{" "}
         <Link href={"https://twitter.com/yamaneko8699/"} className="underline">
           @yamaneko8699
         </Link>
