47cbe6911a803e583d4771b9b1ce81ce02125583
yamaneko86 <yamaneko8699@gmail.com>
2024-02-24 22:12:12 +0900

    ファイル移動によるパスの修正

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import HeaderWithSession from "../_components/HeaderWithSession";
 import Footer from "../_components/Footer";
 
