ebbb107f2400fc0c1d5ca88afc8fd1afa82e0429
yamaneko86 <yamaneko8699@gmail.com>
2024-03-25 00:27:19 +0900

    Biomeによる修正

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,18 +3,18 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url);
-  const formData = await request.formData();
-  const email = String(formData.get("email"));
-  const password = String(formData.get("password"));
-  const supabase = createRouteHandlerClient({ cookies });
+	const requestUrl = new URL(request.url);
+	const formData = await request.formData();
+	const email = String(formData.get("email"));
+	const password = String(formData.get("password"));
+	const supabase = createRouteHandlerClient({ cookies });
 
-  await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+	await supabase.auth.signInWithPassword({
+		email,
+		password,
+	});
 
-  return NextResponse.redirect(requestUrl.origin + "/", {
-    status: 301,
-  });
+	return NextResponse.redirect(`${requestUrl.origin}/`, {
+		status: 301,
+	});
 }
