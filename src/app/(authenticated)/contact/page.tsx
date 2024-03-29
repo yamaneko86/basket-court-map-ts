9d8bc927f9e62dc67de1773f6b9ad8fa7cea0ce6
yamaneko86 <yamaneko8699@gmail.com>
2024-03-29 23:47:49 +0900

    ダミー文章の追加

--- src/app/(authenticated)/contact/page.tsx
+++ src/app/(authenticated)/contact/page.tsx
@@ -6,7 +6,10 @@ export default function Contact() {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel est quos
         voluptas labore repellendus accusamus. Aliquid facere laborum expedita
         numquam reprehenderit maiores tempora nobis nemo. Numquam minima ea
-        explicabo veniam.
+        explicabo veniam. Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Corporis impedit alias dolore eius minus ipsa numquam corrupti
+        aperiam aliquam officiis, praesentium, reiciendis enim ratione similique
+        quae at ex ullam! Asperiores!
       </div>
     </main>
   );
